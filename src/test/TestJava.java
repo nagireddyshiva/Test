b406b7e925a43645ee21aafd155b1b1b7faa4869
dnagireddy <dnagireddy@sitetracker.com>
2020-05-24 15:43:58 -0500

    initial commit

---
+++ src/test/TestJava.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World1");
+System.out.println("Hello World2");
+	}
+
+}
